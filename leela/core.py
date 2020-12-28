401cb4cb30e29dbe3b9bb1e41dadc6b4cbfcca74
Andre Emidio <andresjc2008@gmail.com>
2020-12-27 21:55:34 -0300

    Correção

--- leela/core.py
+++ leela/core.py
@@ -10,6 +10,7 @@ def start(update, context):
                                     text="Oi, Eu sou Leela! Já tem seu chip de profissão?")
 
 def main():
+    PORT = int(os.environ.get('PORT', 5000))
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
@@ -17,7 +18,7 @@ def main():
     dispatcher.add_handler(start_handler)
 
     updater.start_webhook(listen="0.0.0.0",
-                           port=int('$PORT'),
+                           port=int(PORT),
                            url_path=TOKEN)
     updater.bot.setWebhook('https://leela-bot.herokuapp.com/' + TOKEN)
 
